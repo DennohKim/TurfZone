4830c943fe64da212eaa43250d176ef64cfe1f7f
John Kamau <johnjkkamau@gmail.com>
2022-10-11 18:07:17 +0300

    feat:added turves data in seed.rb

--- turfzone-server/db/seeds.rb
+++ turfzone-server/db/seeds.rb
@@ -2,3 +2,53 @@ User.create(username:"John", email:"john@gmail.com", phone_number:"0745412322", 
 User.create(username:"Derrick", email:"der@gmail.com", phone_number:"0756987622", password:"5678jik", password_confirmation:"5678jik")
 User.create(username:"Ivy", email:"ivy@gmail.com", phone_number:"0788968902", password:"ji9089", password_confirmation:"ji9089")
 User.create(username:"Masau", email:"masau@gmail.com", phone_number:"0753463792", password:"b453278h", password_confirmation:"b453278h")
+
+
+Turf.create(image_url: "https://images.unsplash.com/photo-1459865264687-595d652de67e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+name: 'Turf mugoya football',
+location: 'South C',
+rates:4500,
+description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida mi praesent ac sapien nisl, et quis sit.Quis massa tort',
+manager_name:"Charles Mugaka",
+manager_email:"mugaka@gmail.com",
+manager_number:"0756412347")
+Turf.create(image_url: "https://images.unsplash.com/photo-1529900748604-07564a03e7a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+name: 'Kijiji field(Turf)',
+location: 'South B',
+rates: 3500,
+description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida mi praesent ac sapien nisl, et quis sit.Quis massa tort',
+manager_name:"Ruth Wambui",
+manager_email:"boboruth@gmail.com",
+manager_number:"076754321")
+Turf.create(image_url: "https://images.unsplash.com/photo-1536122985607-4fe00b283652?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1234&q=80",
+name: 'The Void Turf',
+location: 'Embakasi',
+rates: 4000,
+description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida mi praesent ac sapien nisl, et quis sit.Quis massa tort',
+manager_name:"Jimmy Bowen",
+manager_email:"Jamiebowe@gmail.com",
+manager_number:"07786412347")
+Turf.create(image_url: "https://images.unsplash.com/photo-1524015368236-bbf6f72545b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+name: 'Ngong Road Arena',
+location: 'Ngong Road',
+rates: 4500,
+description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida mi praesent ac sapien nisl, et quis sit.Quis massa tort',
+manager_name:"Jeremiah Kiunu",
+manager_email:"Kjeremy@gmail.com",
+manager_number:"0776857697")
+Turf.create(image_url: "https://images.unsplash.com/photo-1550881111-7cfde14b8073?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+name: 'Camp Toyoyo',
+location: 'Parklands',
+rates: 4000,
+description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida mi praesent ac sapien nisl, et quis sit.Quis massa tort'
+manager_name:"Mary Sisia",
+manager_email:"Sisiamary@gmail.com",
+manager_number:"0711028399")
+Turf.create(image_url: "https://images.unsplash.com/photo-1518604666860-9ed391f76460?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+name: 'Arena 5 Astroturf - Jamhuri Primary',
+location: 'Jamhuri',
+rates: 3500,
+description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida mi praesent ac sapien nisl, et quis sit.Quis massa tort',
+manager_name:"Gideon Kinyanjui",
+manager_email:"giddykinyash@gmail.com",
+manager_number:"0760002341")
