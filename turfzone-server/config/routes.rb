9d4da9d4b61b5720754706160798ededd6179803
John Kamau <johnjkkamau@gmail.com>
2022-10-11 14:21:45 +0300

    feat: run migrations

--- turfzone-server/config/routes.rb
+++ turfzone-server/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  resources :users, only: [:index]
 end
