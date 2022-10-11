70e337725b67fa84838450788d28340c03fc0e48
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-10-11 16:23:29 +0300

    style: changed text to black for visiblity.

--- turfzone-client/src/components/Footer/Footer.jsx
+++ turfzone-client/src/components/Footer/Footer.jsx
@@ -5,29 +5,29 @@ const Footer = () => {
     <div className='flex flex-col items-center justify-center h-full bg-color'>
         <div className='flex flex-row items-center justify-between w-full px-4 py-4'>
             <div className='flex flex-col items-center justify-center'>
-                <h1 className='text-3xl font-bold text-white'>Talk to us</h1>
-                <p className='text-lg text-white'>+254 234 5678</p>
-                <p className='text-lg text-white'>
+                <h1 className='text-3xl font-bold text-black'>Talk to us</h1>
+                <p className='text-lg text-black'>+254 234 5678</p>
+                <p className='text-lg text-black'>
                 <a href='mailto:someone@gmail.com'>Send Email</a>
                 </p>
-                <p className='text-lg text-white'>1234 TurfZone Street, Nairobi 00100</p>
+                <p className='text-lg text-black'>1234 TurfZone Street, Nairobi 00100</p>
             </div>
             <div className='flex flex-col items-center justify-center'>
-                <h1 className='text-3xl font-bold text-white'>Quick Links</h1>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Home</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Turf Playgrounds</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>About Us</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Testimonials</a>
+                <h1 className='text-3xl font-bold text-black'>Quick Links</h1>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Home</a>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Turf Playgrounds</a>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>About Us</a>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Testimonials</a>
             </div>
             <div className='flex flex-col items-center justify-center'>
-                <h1 className='text-3xl font-bold text-white'>Social Media</h1>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Facebook</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Instagram</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Twitter</a>
+                <h1 className='text-3xl font-bold text-black'>Social Media</h1>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Facebook</a>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Instagram</a>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Twitter</a>
             </div>
         </div>
         <div className='flex flex-row items-center justify-center w-full px-4 py-4'>
-        <p className='text-lg text-white'>© 2022 TurfZone. All rights reserved.</p>
+        <p className='text-lg text-black'>© 2022 TurfZone. All rights reserved.</p>
         </div>
     </div>
 );
--- turfzone-client/src/components/HeroSection/HeroSection.jsx
+++ turfzone-client/src/components/HeroSection/HeroSection.jsx
@@ -5,14 +5,14 @@ const HeroSection = () => {
     <div className='flex flex-col items-center justify-center h-full'>
     <div className='flex flex-row items-center justify-between w-full px-4 py-4'>
       <div className='flex flex-row items-center justify-center'>
-        <img src='https://www.figma.com/file/BGJQUi8O4Hcrzglhx2M3pL/Sports-Turf-Booking-App?node-id=132%3A215' alt='TurfZone Logo' className='w-16 h-16' />
+        <img src='../../assets/images/logo.png' alt='TurfZone Logo' className='w-16 h-16' />
         <h1 className='text-3xl font-bold text-white'>TurfZone</h1>
       </div>
       <div className='flex flex-row items-center justify-center'>
-        <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Home</a>
-        <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Featured Playgrounds</a>
-        <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Why TurfZone</a>
-        <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Testimonials</a>
+        <a href='#' className='px-4 py-2 text-lg font-bold text-black '>Home</a>
+        <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Featured Playgrounds</a>
+        <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Why TurfZone</a>
+        <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Testimonials</a>
       </div>
       <div className='flex flex-row items-center justify-center'>
         <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-pink-500 rounded-md hover:bg-pink-600'>Customer Login</a>
@@ -20,15 +20,15 @@ const HeroSection = () => {
       </div>
     </div>
     <div className='flex flex-col items-center justify-center w-full h-full'>
-      <img src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617670009/turfzone/footballer.png' alt='Footballer' className='w-1/2 h-1/2' />
-      <h1 className='text-4xl font-bold text-white'>Connecting the turfs</h1>
-      <p className='text-lg text-white'>Turf zone is a sportsground and outdoor playground booking portal that provides easy booking facility for schools, clubs and other sports organizations
-        across Kenya. Enabling them to easily book a turf playground online - anytime,
-        anywhere.
+      <img src='../../assets/images/footballer1.png' alt='Footballer' className='w-1/2 h-1/2' />
+      <h1 className='text-4xl font-bold text-black'>Connecting the turfs</h1>
+      <p className='text-lg text-black'>Turf zone is a sportsground and outdoor playground booking portal that provides easy booking facility for schools, clubs and other sports organizations
+        across Kenya. Enabling them to easily book a turf playground online - anytime, anywhere.
       </p>
-      <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600'>Book Turf</a>
+      <a href='#' className='px-4 py-2 text-lg font-bold text-white bg-pink-500 rounded-md hover:bg-pink-600'>Book Turf</a>
     </div>
-  </div>  );
+  </div>
+  );
 }
 
 export default HeroSection;
