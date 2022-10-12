3136a1a1e2c716e50bba686712f291834088e6c4
John Kamau <johnjkkamau@gmail.com>
2022-10-12 13:41:15 +0300

    feat: Added auto login feature

--- turfzone-server/app/controllers/users_controller.rb
+++ turfzone-server/app/controllers/users_controller.rb
@@ -18,23 +18,23 @@ class UsersController < ApplicationController
     end
 
     # Auto login a user if they refresh page
-    # def show
-    #     user = User.find_by(id: session[:user_id])
-    #     if user 
-    #         render json: user
-    #     else
-    #         render json: {error: "Not authorized"}, status: :unauthorized
-    #     end
-    # end 
-
     def show
-        user = User.find(params[:id])
-        if user
-          render json: user
+        user = User.find_by(id: session[:user_id])
+        if user 
+            render json: user
         else
-          render json: { error: "user not found" }, status: :not_found
+            render json: {error: "Not authorized"}, status: :unauthorized
         end
-    end
+    end 
+
+    # def show
+    #     user = User.find(params[:id])
+    #     if user
+    #       render json: user
+    #     else
+    #       render json: { error: "user not found" }, status: :not_found
+    #     end
+    # end
 
     private
 
