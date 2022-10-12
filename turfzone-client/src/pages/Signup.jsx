939d709d56989b63ce849fc7f6c8dd026ec0a4eb
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-12 16:00:39 +0300

    feat: sign up a new user

--- turfzone-client/src/pages/Signup.jsx
+++ turfzone-client/src/pages/Signup.jsx
@@ -6,19 +6,68 @@ import { Link, useNavigate } from "react-router-dom";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setError("");
+  //   try {
+  //     await signIn(email, password);
+  //     navigate("/dashboard");
+  //   } catch (e) {
+  //     setError(e.message);
+  //   }
+  // };
+
+  // function handleSubmit(event){
+  //   event.preventDefault();
+
+  //   const addLandForm = { description: formData.description,
+  //   reference: formData.reference,
+  //   value: formData.value,
+  //   size: formData.size,
+  //   location: formData.location,
+  //   date: formData.date,
+  //   details:formData.details,}
+
+  //   fetch(landUrl, {
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/json"
+  //     },
+  //     body: JSON.stringify(addLandForm)
+
+  //   })
+  //   .then(response => response.json())
+  //   .then(newLandComparable => {
+  //     onAddLand(newLandComparable)
+  //     setFormData({...formData,  description: "", reference: "", value:"", size:"", location:"",date:"" })
+  //   })
+  // }
+
+  function handleSubmit(e){
     e.preventDefault();
-    setError("");
-    try {
-      await signIn(email, password);
-      navigate("/dashboard");
-    } catch (e) {
-      setError(e.message);
-    }
-  };
+    fetch("http://localhost:3000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        phone_number: phoneNumber,
+        password,
+        password_confirmation: passwordConfirmation,
+      })
+    })
+    .then((r) => r.json())
+      .then((r) => console.log(r))
+  }
 
   return (
     <>
@@ -36,7 +85,7 @@ const Signup = () => {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
+            <form  onSubmit={handleSubmit} className="space-y-6">
             <div>
                 <label
                   htmlFor="username"
@@ -48,7 +97,9 @@ const Signup = () => {
                   <input
                     id="username"
                     name="username"
+                    onChange={e => setUsername(e.target.value)}
                     type="text"
+                    value={username}
                     autoComplete="username"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-color focus:border-primary-color sm:text-sm"
@@ -68,6 +119,8 @@ const Signup = () => {
                   <input
                     id="email"
                     name="email"
+                    onChange={e => setEmail(e.target.value)}
+                    value={email}
                     type="email"
                     autoComplete="email"
                     required
@@ -87,7 +140,9 @@ const Signup = () => {
                   <input
                     id="Phone-number"
                     name="Phone Number"
-                    type="number"
+                    value={phoneNumber}
+                    onChange={e => setPhoneNumber(e.target.value)}
+                    type="text"
                     autoComplete="0712 234 567"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-color focus:border-primary-color sm:text-sm"
@@ -106,6 +161,8 @@ const Signup = () => {
                   <input
                     id="password"
                     name="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                     type="password"
                     autoComplete="current-password"
                     required
@@ -125,6 +182,8 @@ const Signup = () => {
                   <input
                     id="password-confirmation"
                     name="password-confirmation"
+                    value={passwordConfirmation}
+                    onChange={e => setPasswordConfirmation(e.target.value)}
                     type="password"
                     autoComplete="current-password"
                     required
@@ -138,6 +197,7 @@ const Signup = () => {
               <div>
                 <button
                   type="submit"
+                  
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-color hover:bg-primary-color focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-color"
                 >
                   Sign up
