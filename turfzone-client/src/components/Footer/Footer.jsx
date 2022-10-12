72914d165ba602776aaac767347038db1bd89079
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-10-12 11:22:27 +0300

    style: changed the formatting of certain elements.

--- turfzone-client/src/components/Footer/Footer.jsx
+++ turfzone-client/src/components/Footer/Footer.jsx
@@ -2,32 +2,32 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-full bg-color'>
+    <div className='flex flex-col items-center justify-center bg-purple'>
         <div className='flex flex-row items-center justify-between w-full px-4 py-4'>
             <div className='flex flex-col items-center justify-center'>
-                <h1 className='text-3xl font-bold text-black'>Talk to us</h1>
-                <p className='text-lg text-black'>+254 234 5678</p>
-                <p className='text-lg text-black'>
+                <h1 className='text-3xl font-bold pink-text'>Talk to us</h1>
+                <p className='text-md text-white'>+254 234 5678</p>
+                <p className='text-lg text-white'>
                 <a href='mailto:someone@gmail.com'>Send Email</a>
                 </p>
-                <p className='text-lg text-black'>1234 TurfZone Street, Nairobi 00100</p>
+                <p className='text-lg text-white'>1234 TurfZone Street, Nairobi 00100</p>
             </div>
             <div className='flex flex-col items-center justify-center'>
-                <h1 className='text-3xl font-bold text-black'>Quick Links</h1>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Home</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Turf Playgrounds</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>About Us</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Testimonials</a>
+                <h1 className='text-3xl font-bold pink-text'>Quick Links</h1>
+                <a href='#' className='px-4 py-2 text-md font-bold text-white'>Home</a>
+                <a href='#' className='px-4 py-2 text-md font-bold text-white'>Turf Playgrounds</a>
+                <a href='#' className='px-4 py-2 text-md font-bold text-white'>About Us</a>
+                <a href='#' className='px-4 py-2 text-md font-bold text-white'>Testimonials</a>
             </div>
             <div className='flex flex-col items-center justify-center'>
-                <h1 className='text-3xl font-bold text-black'>Social Media</h1>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Facebook</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Instagram</a>
-                <a href='#' className='px-4 py-2 text-lg font-bold text-black'>Twitter</a>
+                <h1 className='text-3xl font-bold pink-text'>Social Media</h1>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-white'>Facebook</a>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-white'>Instagram</a>
+                <a href='#' className='px-4 py-2 text-lg font-bold text-white'>Twitter</a>
             </div>
         </div>
         <div className='flex flex-row items-center justify-center w-full px-4 py-4'>
-        <p className='text-lg text-black'>© 2022 TurfZone. All rights reserved.</p>
+        <p className='text-lg text-white'><span className="pink-text">© 2022 TurfZone</span> All rights reserved.</p>
         </div>
     </div>
 );
