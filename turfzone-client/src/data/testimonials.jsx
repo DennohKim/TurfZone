7cc6550c35d2a783d2e2e562ca9aada2821788b2
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-17 10:49:16 +0300

    fix: avatar image url

--- turfzone-client/src/data/testimonials.jsx
+++ turfzone-client/src/data/testimonials.jsx
@@ -1,38 +1,38 @@
 
 export const testimonialInfo = [
     {
-      image: "https://i.ibb.co/fXQ3gj2/Ellipse-1.png",
+      image: "https://user-images.githubusercontent.com/63696529/196117661-57f87a6f-2155-4328-8b48-fa27d6ec85a2.png",
       quote: '“Let us help you find a suitable time for your team to practice or play on the turf.”',
       name: 'Abdrizak Hamza',
       
     },
 
     {
-        image: "https://i.ibb.co/M9mbDg3/Ellipse-2.png",
+        image: "https://user-images.githubusercontent.com/63696529/196118175-d8a7884b-8be5-4b21-be9a-23ed78b187be.png",
         quote: '“Turf Zone makes it extremely easy to book the training ground. You just have to search for the available dates and then book it. All the information is provided on the website and even if you have any questions in mind, they provide a phone number where they can be reached at any time.”',
         name: 'Collins Omwami',
     },
 
     {
-        image: "https://i.ibb.co/5c0LCH1/Ellipse-3.png",
+        image: "https://user-images.githubusercontent.com/63696529/196118387-e3852151-5f7d-4426-8416-f8b679f77e17.png",
         quote: '“TurfZone booking is a great way to reserve some time on the sports field. With turf zone, you can book the time on your favourite sports ground whenever you want. Its easy, quick, and efficient.”',
         name: 'Evans Ruto',
     },
 
     {
-        image: "https://i.ibb.co/dDMCbMg/Ellipse-4.png",
+        image: "https://user-images.githubusercontent.com/63696529/196118581-24c47276-aecb-43d3-bef1-13c5cfa32371.png",
         quote: '“Turf Zone is a great platform because it helps make the process of booking sports grounds much more efficient.”',
         name: 'Gift Randu',
     },
 
     {
-        image: "https://i.ibb.co/HT0Kdr9/Ellipse-5.png",
+        image: "https://user-images.githubusercontent.com/63696529/196118716-4a309b8f-faec-4ba4-9821-01a56bb07481.png",
         quote: '“I have to admit this is a great idea and Ive been very impressed with how easy it is to use. I also like how you can book for a specific time slot as opposed to just being available for hire.”',
         name: 'Eazzy',
     },
 
     {
-        image: "https://i.ibb.co/zNfS0Xp/Ellipse-6.png",
+        image: "https://user-images.githubusercontent.com/63696529/196118977-fe6c9b64-b1e8-4cb6-bac9-79ac7efa23a6.png",
         quote: '“Turf Zone is the best turf booking website for sport ground. It makes the process of booking the playing field easy and efficient.”',
         name: 'Mary wambui',
     },
