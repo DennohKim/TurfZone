84d41cbef37c9557c2dd257b35f1e0bcb94ceb5a
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-17 10:21:30 +0300

    fix: cross origin error

--- turfzone-server/config/initializers/cors.rb
+++ turfzone-server/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:5173'
+      origins 'https://turfzone.vercel.app'
   
       resource '*',
         headers: :any,
